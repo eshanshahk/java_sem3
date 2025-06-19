f6f6ea2ea99a590e0a0f064e05ec7619ff7ff0e3
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-06-19 19:57:29 +0530

    Update one.java

--- one.java
+++ one.java
@@ -1,8 +1,8 @@
 public class one {
     public static void main(String[] args) 
     {
-        int a=4,b=,c;
-        a=c;
+        int a=4,b=5,c;
+        c=a;
         a=b;
         b=c;
         System.out.println("A= %d B=%d"+a+b);
