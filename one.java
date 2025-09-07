b61787c55c757aa3c739757fa93a712ca6ffd700
eshanshahk <eshanshahk123@gmail.com>
2025-09-07 13:26:03 +0530

    update

--- one.java
+++ one.java
@@ -1,12 +1,12 @@
+
 public class one {
-    public static void main(String[] args) 
-    {
-        int a=4,b=5,c;
-        c=a;
-        a=b;
-        b=c;
-        int d;
-        
-        System.out.println("A="+a+ "B="+b);
+
+    public static void main(String[] args) {
+        int a = 4, b = 5, c;
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("A=" + a + "B=" + b);
     }
 }
