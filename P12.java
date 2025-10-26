ac86185094ede33c4d1274dd1c6fbe2b3220a97a
eshanshahk <eshanshahk123@gmail.com>
2025-10-26 14:41:33 +0530

    p13

--- P12.java
+++ P12.java
@@ -1,10 +1,26 @@
-
-import java.io.IOException;
-
 //Write a Java program that displays the number of characters, lines and words in a text.
 
-class P12{
-    public static void main(String[] args) throws IOException{
-        para;
+import java.io.*;
+
+class P12 {
+
+    public static void main(String[] args) throws IOException {
+        DataInputStream dis = new DataInputStream(System.in);
+        System.out.println("Enter text(Press Enter on an empty line to finish!):");
+        int wordCount = 0, lineCount = 0, charCount = 0;
+
+        while (true) {
+            String para = dis.readLine();
+            if (para.length() == 0) {
+                break;
+            }
+            lineCount++;
+            charCount += para.length();
+            String Words[] = para.trim().split("\\s+");
+            wordCount += Words.length;
+        }
+        System.out.println("Lines = " + lineCount);
+        System.out.println("Characters = " + charCount);
+        System.out.println("Words = " + wordCount);
     }
 }
---
+++ P13.java
@@ -0,0 +1,31 @@
+
+/*WRITE A JAVA PROGRAM TO CREATE A CLASS CALLED SHAPE WITH METHODS CALLED GETPERIMETER() AND GETAREA(). 
+CREATE A SUBCLASS CALLED CIRCLE THAT OVERRIDES THE GETPERIMETER() AND GETAREA() METHODS TO 
+CALCULATE THE AREA AND PERIMETER OF A CIRCLE.*/
+
+import java.io.*;
+
+class Shape{
+    double getPerimeter(){
+        return 0;
+    }
+    double getArea(){
+        return 0;
+    }
+}
+
+class Circle extends Shape{
+    double getPerimeter(int r){
+        return 2*3.14*r;
+    }
+    double getArea(int r){
+        return 3.14*r*r;
+    }
+}
+class P13{
+    public static void main(String[] args) throws IOException {
+    Circle obj=new Circle();
+    System.out.println("Perimeter: "+obj.getPerimeter(5));
+    System.out.println("Area : "+obj.getArea(5));
+    }
+}
